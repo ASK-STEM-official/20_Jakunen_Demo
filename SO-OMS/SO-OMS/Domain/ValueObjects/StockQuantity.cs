d389ec2448faaa85c6cee9c95d92791f5b65eee4
OMEGA <lightsound2321@gmail.com>
2025-07-17 13:46:53 +0900

    お金、アラートしきい値、在庫数のVOを追加

---
+++ SO-OMS/SO-OMS/Domain/ValueObjects/StockQuantity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public readonly struct StockQuantity
+{
+    public int Value { get; }
+
+    public StockQuantity(int value)  // コンストラクタ
+    {
+        if (value < 0)
+            throw new ArgumentException("在庫数は0以上である必要があります。");
+        Value = value;
+    }
+
+    public override string ToString() => Value.ToString();
+
+    public static implicit operator int(StockQuantity quantity) => quantity.Value;
+    public static explicit operator StockQuantity(int value) => new StockQuantity(value);
+}
